3b5984f6e060df425aa1ae9e048e41e968900049
marcosjunior <marcosjunior-me@ufu.br>
2025-09-17 18:25:32 -0300

    alterando deck

--- src/main/kotlin/domain/Deck.kt
+++ src/main/kotlin/domain/Deck.kt
@@ -1,17 +1,14 @@
 package com.estudoapp.domain
 
-import com.google.firebase.database.IgnoreExtraProperties
+
 import kotlinx.serialization.Serializable
 
-// que não existem nesta classe, apenas ignore-os em vez de dar erro".
-@IgnoreExtraProperties
-// @Serializable é do Kotlinx. Ele é usado pelo Ktor para converter este objeto
-// para JSON na hora de enviar a resposta para o Postman/App.
+
 @Serializable
 data class Deck(
-    val id: String = "",
-    val name: String = "",
-    val description: String = "",
-    val userId: String = "",
-    val cardCount: Int = 0
+    var id: String = "",
+    var name: String = "",
+    var description: String = "",
+    var userId: String = "",
+    var cardCount: Int = 0
 )
